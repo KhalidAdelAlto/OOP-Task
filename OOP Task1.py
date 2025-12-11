934fe8e998e01cab56b80ebe09008b9d647dc391
KhalidAdelAlto <132235205+KhalidAdelAlto@users.noreply.github.com>
2025-12-11 11:12:42 +0300

    OOP Task1.py

--- OOP Task1.py
+++ OOP Task1.py
@@ -1,11 +1,13 @@
 #Khalid Althomali 
 #OOP Practice
 
+# Parent class
+# Encabsulation and Abstraction 
 class Vehicle:
     def sound(self):
         raise NotImplementedError("Subclasses must implement this")
 
-
+# Car inherits from Vehicle
 class Car(Vehicle):
     def __init__(self):   # <-- FIXED
         self.brand = "Toyota"
@@ -19,4 +21,5 @@ class Car(Vehicle):
 car = Car()
 print(car.brand, car.model)   # Toyota Camry
 print(car.sound())            # Vroom!
+
 
